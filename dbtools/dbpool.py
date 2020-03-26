0b132ae5270e0cebe7a406f5d9095bd76b247557
MangoPro <zengmiaokun@live.com>
2020-03-27 01:36:15 +0800

    add mysql_tools

--- dbtools/dbpool.py
+++ dbtools/dbpool.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 """
 Create pool and operate database using DBUtils
 """
@@ -11,9 +11,9 @@ class mdpool():
     def __init__(self, host, user, passwd, database, port = 3306):
         self.pool = PooledDB(
             creator=MySQLdb,  # Database connection driver
-            maxconnections=6,  # Maximum number of connections allowed. `0` and `None` means unlimited
-            mincached=8,  # At initialization, at least free links created in the link pool. `0` means not to create
-            maxcached=10,  # Maximun number of  idle links in the connection pool. `0` and `None` means unlimited
+            maxconnections=0,  # Maximum number of connections allowed. `0` and `None` means unlimited
+            mincached=10,  # At initialization, at least free links created in the link pool. `0` means not to create
+            maxcached=20,  # Maximun number of  idle links in the connection pool. `0` and `None` means unlimited
             maxusage=None,  # Maximum number of times a connection has been reused. `None` means unlimited
             host=host,
             port=int(port),
@@ -23,13 +23,13 @@ class mdpool():
             charset='utf8'
         )
 
-    def create_conn_cursor(self):
+    def get_conn_cursor(self):
         conn = self.pool.connection()
         cursor = conn.cursor()
         return conn, cursor
 
     def fetch_one(self, sql, args=()):
-        conn, cursor = self.create_conn_cursor()
+        conn, cursor = self.get_conn_cursor()
         cursor.execute(sql, args)
         result = cursor.fetchone()
         cursor.close()
@@ -37,7 +37,7 @@ class mdpool():
         return result
 
     def fetch_all(self, sql, args=()):
-        conn, cursor = self.create_conn_cursor()
+        conn, cursor = self.get_conn_cursor()
         cursor.execute(sql, args)
         result = cursor.fetchall()
         cursor.close()
@@ -45,7 +45,7 @@ class mdpool():
         return result
 
     def insert_one(self, sql, args=()):
-        conn, cursor = self.create_conn_cursor()
+        conn, cursor = self.get_conn_cursor()
         res = cursor.execute(sql, args)
         conn.commit()
         print(res)
@@ -53,7 +53,7 @@ class mdpool():
         return res
 
     def update(self, sql, args=()):
-        conn, cursor = self.create_conn_cursor()
+        conn, cursor = self.get_conn_cursor()
         res = cursor.execute(sql, args)
         conn.commit()
         print(res)
@@ -62,9 +62,4 @@ class mdpool():
 
 
 if __name__ == "__main__":
-    dbconfig = {'host': 'localhost', 'user': 'test', 'passwd': 'password', 'port': 3306, 'database': 'corpus'}
-    testdb = mdpool(**dbconfig)
-    sql = r"SELECT * FROM `articles`"
-    data = testdb.fetch_all(sql)
-    for i in data:
-        print(i)
+    pass
